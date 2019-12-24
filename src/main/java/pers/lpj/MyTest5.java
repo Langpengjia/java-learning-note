158e3abe2f9b62f836770ad9d4e6703ed71920da
Desire <562457731@qq.com>
2019-12-24 10:41:18 +0800

    子类或者子接口的主动使用不会导致其父接口的加载和初始化

--- src/main/java/pers/lpj/MyTest5.java
+++ src/main/java/pers/lpj/MyTest5.java
@@ -18,28 +18,51 @@ public class MyTest5 {
          *
          * 很不好验证
          */
-        System.out.println(MyChild5.b);
+//        new C();
+//        new C();
+//
+//        System.out.println(MyChild5.b);
+
+        System.out.println(MyParent5_1.THREAD);
+
+
     }
 
 }
 
+class MyCrandPa {
 
-interface MyParent5 {
+    public static Thread THREAD = new Thread() {{
+
+        System.out.println("My MyCrandPa thread ! ");
+
+    }};
+}
+
+
+class MyParent5 extends MyCrandPa {
+
+    public static Thread THREAD = new Thread() {{
+
+        System.out.println("My Parent5 thread5 ! ");
+
+    }};
+
 
     /**
      * 接口中的变量都是常量 public static final
      */
-//    public static int a = 10;
-    int a = 10;
+    //    public static int a = 10;
+//    int a = 10;
 
 }
 
-interface MyChild5 extends MyParent5 {
+class MyChild5 extends MyParent5 {
 
     /**
      * 存在于常量池中
      */
-    int b = 5;
+    public static int b = 5;
     /**
      * -----------------------------
      * 但是，如果变量是由复杂运算生成的，
@@ -50,3 +73,45 @@ interface MyChild5 extends MyParent5 {
 
 
 }
+
+interface MyParent5_1 {
+    public static Thread THREAD = new Thread() {{
+        System.out.println("My Parent5_1 thread5 ! ");
+    }};
+
+}
+
+
+interface MyCrand5_1 {
+    public static Thread THREAD = new Thread() {{
+        System.out.println("My Crand5_1 thread5 ! ");
+    }};
+
+}
+
+
+
+class C {
+    static {
+        System.out.println(" static block !");
+
+    }
+
+
+    public C() {
+        System.out.println(" class C Constractor!");
+    }
+
+    {
+        System.out.println("this is only a test!");
+    }
+
+}
+
+
+/**
+ * 一个类被初始化，他的所有父类会被初始化，他的父接口除非被主动使用，否者不会被初初始化
+ * 一个接口的初始化，不会导致他的父接口的初始化
+ * <p>
+ * 只有简单的final量才会被直接加载到调用改常量的类中，这意味着，调用类和常量所在的类没有任何关系
+ */
